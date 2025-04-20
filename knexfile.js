370007270324d4a3fdf6c4b7ef706ff243c6023f
Eneson <enersonsony7@gmail.com>
2025-04-20 16:37:06 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,10 @@
 module.exports = {
     client: 'mysql',
     connection: {
-        host : 'r98du2bxwqkq3shg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        host : 'HOSTBD',
         port : 3306,
-        user : 'kt300jk09xfh6hwb',
-        password : 'g2enom8u62x36sok',      
-        database : 'ul0soh4469aff5t0',
+        user : 'USERBD',
+        password : 'SENHABD',      
+        database : 'DATASEBD',
       }
 };
