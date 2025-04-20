fbad71b16ba042726e16a8410acd243b8946bc92
Eneson <enersonsony7@gmail.com>
2025-04-20 16:13:54 -0300

    Add files via upload

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+// Update with your config settings.
+
+module.exports = {
+    client: 'mysql',
+    connection: {
+        host : 'r98du2bxwqkq3shg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        port : 3306,
+        user : 'kt300jk09xfh6hwb',
+        password : 'g2enom8u62x36sok',      
+        database : 'ul0soh4469aff5t0',
+      }
+};
